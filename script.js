61425692bde8dbe220d81288763debba27ace753
dvsantos10s-hue <d.vsantos10s@gmail.com>
2026-08-09 13:56:45 -0300

    Adicionar conexão com supabase

---
+++ script.js
@@ -0,0 +1,25 @@
+const SUPABASE_URL = "https://qebsemnlhikhmucyjdut.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_1dB6oHhncjKZVO4utmaU3w_d_pxTQiz";
+
+const db = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function registrarVotos(nomes) {
+  const votos = nomes.map(nome => ({
+    eleitor: "anonimo",
+    membro: nome
+  }));
+
+  const { error } = await db
+    .from("votos")
+    .insert(votos);
+
+  if (error) {
+    console.error("Erro ao registrar votos:", error);
+    return false;
+  }
+
+  return true;
+}
